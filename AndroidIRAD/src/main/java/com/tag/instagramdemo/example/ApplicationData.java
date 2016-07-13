74bba5051da5b39ecf4a33df52d5a2e0c3adf12a
Zack Keiser <zachary.m.keiser@saic.com>
2016-07-13 15:44:28 -0400

    First commit, somewhat working.

---
+++ AndroidIRAD/src/main/java/com/tag/instagramdemo/example/ApplicationData.java
@@ -0,0 +1,7 @@
+package com.tag.instagramdemo.example;
+
+public class ApplicationData {
+	public static final String CLIENT_ID = "5518bafef7c3464f9041a0b2b9b5f1bb";
+	public static final String CLIENT_SECRET = "33eef95177c24c6a816ec7f03f699a72";
+	public static final String CALLBACK_URL = "instagram://connect";
+}
